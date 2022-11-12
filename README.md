0716e4243df0447774f85b3eeaba0adedd716a1d
fjnelson <97076779+fjnelson@users.noreply.github.com>
2022-11-11 20:10:19 -0800

    coding

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Module4Challenge
+# Module4Challenge - Web APIs Challenge
+
+## Description
+
+Bootcamp: Module 4 Challenge <br />
+Web APIs Challenge - timed quiz
+Codebase correction that follows accessibility standards so that the site is optimized for search engines
+
+## Table of Contents (Optional)
+
+N/A
+
+## Installation
+
+Github pages hosting: placeholder
+
+## Usage
+
+![alt text](placeholder)
+
+## Credits
+
+Placeholder
+
+## License
+
+Please refer to the LICENSE in the repo.
---
+++ assets/js/questions.js
@@ -0,0 +1,38 @@
+//questions
+const questions = [
+    {
+        question: "What are various data types that exist in JavaScript",
+        choices: ["a. Boolean", "b. Number", "c. String", "d. All the above"],
+        answer: "d. All the above"
+    },
+    {
+        question: "How do you create an array in JavaScript",
+        choices: ["a. a = []", "b. a = ()", "c. a = <>", "a = ||"],
+        answer: "a. a = []"
+    },
+    {
+        question: "What are some JavaScript frameworks",
+        choices: ["a. React", "b. Angular", "c. Node", "d. All of the above"],
+        answer: "d. All the above"
+    },
+    {
+        question: "What are some of the methods in JavaScript",
+        choices: ["a. confirm()", "b. alert()", "c. close()", "d. All the above"],
+        answer: "d. All the above"
+    },
+    {
+        question: "How do you create a function in JavaScript",
+        choices: ["a. function => myFunction()", "b. function myFunction()", "c. function===myFunction()", "d. MyFunction()"],
+        answer: "b. function myFunction()"
+    },
+    {
+        question: "How do you get a value passed into the browser console?",
+        choices: ["a. console.show()", "b. console.get()", "c. console.log()", "d. console.javascript()"],
+        answer: "c. console.log()"
+    },
+    {
+        question: "What value is in the 5th index in array [0, 1, 2, 3, 4, 5, 6, 7]",
+        choices: ["a. 3", "b. 4", "c. 5", "d. 6"],
+        answer: "b. 4"
+    }
+]
